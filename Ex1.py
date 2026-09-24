552e56e01746b1e4e64eec3f0ce30ce7314852e5
Juan <bellottijuan3@gmail.com>
2026-09-24 12:04:11 -0300

    Maior e menor

--- Ex1.py
+++ Ex1.py
@@ -1,19 +1,18 @@
 num1 = 0
 num = 0
-
 def MaiorMenor():
     if num1 > num2:
-        print(f"{num1} é maior que {num2}")
+        d = num1 - num2
+        print(f"{num1} é maior que {num2} a diferença entre eles é {d}")
     else:
-        print(f"{num2} é maior que {num1}")
+        d = num2 - num1
+        print(f"{num2} é maior que {num1} a diferença entre eles é {d}")
     
-    
+     
 def main():
     global num1, num2
     num1 = int(input("Digite o primeiro numero: "))
     num2 = int(input("Digite o segundo numero: "))
     MaiorMenor()
-
-
 
 main()
