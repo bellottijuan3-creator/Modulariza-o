c665d8755c4ea3313b14a9b752bd9a7bcb906d5c
Juan <bellottijuan3@gmail.com>
2026-09-24 12:15:01 -0300

    Maior numero

---
+++ Ex2.py
@@ -0,0 +1,19 @@
+num1 = 0
+num2 = 0
+
+def CalcularMaior():
+    if num1 > num2:
+        print(f"{num1} é maior que {num2}")
+    else:
+        print(f"{num2} é maior que {num1}")
+    
+    
+    
+def main():
+    global num1, num2
+    num1 = float(input("Digite o primeiro numero: "))
+    num2 = float(input("Digite o segundo numero: "))
+    CalcularMaior()
+
+main()
+print("carlos")
